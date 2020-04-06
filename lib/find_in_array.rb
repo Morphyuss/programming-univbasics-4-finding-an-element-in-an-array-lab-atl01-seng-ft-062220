86ddf92fa9a28b2d65eca34e3e31980d3cdc75ee
James Stamps Jr <jstampshere@gmail.com>
2020-04-06 13:01:22 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,16 +1,6 @@
 
 
 def find_element_index(array, value_to_find)
-  while array < array.length |index| do
-    puts array[index]
-  else
-    puts NIL
-  end
-end
-
-
-
-def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
     if array[count] == value_to_find
@@ -19,3 +9,4 @@ def find_element_index(array, value_to_find)
     count += 1
   end
 end
+
